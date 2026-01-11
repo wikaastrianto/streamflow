0a9721c7cc4c96c4eb379287a26b3f522cfa33d6
Wika Astrianto <145605667+wikaastrianto@users.noreply.github.com>
2026-01-11 14:43:28 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ else
 fi
 
 echo "📥 Clone repository..."
-git clone https://github.com/bangtutorial/streamflow
+git clone https://github.com/wikaastrianto/streamflow
 cd streamflow
 
 echo "⚙️ Installing dependencies..."
